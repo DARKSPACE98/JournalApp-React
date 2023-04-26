d71e9d84a689fd332b30540582f7f9b967aa88d6
DARKSPACE98 <enriquechuyin2019@gmail.com>
2023-04-26 00:08:10 -0700

    Se agrega diseño de pantalla RegisterPage Parte 3

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,24 +1,13 @@
 import { Link as RouterLink } from 'react-router-dom';
 import { Google } from "@mui/icons-material";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from '../layout/AuthLayout';
 
 
 export const LoginPage = () => {
   return (
-    <Grid 
-    container
-    spacing={0}
-    direction="column"
-    alignContent="center"
-    justifyContent="center"
-    sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4}}
-    >
-    <Grid item
-    className="box-shadow"
-    xs={ 3 }
-    sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2}}>
-      <Typography variant="h5" sx={{ mb: 1 }}>Login</Typography>
-      
+    <AuthLayout title='Login'>
+
       <form>
         <Grid  container>
           <Grid item xs={12} sx={{mt: 2}}>
@@ -68,9 +57,8 @@ export const LoginPage = () => {
 
       </form>
 
-
-      </Grid>
-
-    </Grid>
+    </AuthLayout>
+    
+ 
   )
 }
