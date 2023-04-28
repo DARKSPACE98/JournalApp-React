058b9f8d69935df919aa5b398401c80b7e731808
DARKSPACE98 <enriquechuyin2019@gmail.com>
2023-04-27 23:51:24 -0700

    Se agrega diseño de pantalla principal del JournalApp

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,15 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 
 export const JournalPage = () => {
   return (
-    <>
-    <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+    <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque optio, veniam vel rem dicta alias quibusdam cupiditate mollitia ducimus voluptatem dignissimos delectus vitae sequi, quo animi beatae saepe ullam cum.</Typography>
+    
+    {/* NothingSelected */}
+    {/*  Noteview */}
+
+    </JournalLayout>
   )
 }
