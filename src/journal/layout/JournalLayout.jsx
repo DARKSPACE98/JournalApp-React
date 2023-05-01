166ea66f17f05133d5e5ea0b4650206c1145517f
DARKSPACE98 <enriquechuyin2019@gmail.com>
2023-05-01 14:40:34 -0700

    Se agrega diseño de la SlideBar de JournalApp Parte 5

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,7 +1,7 @@
-import { Box } from "@mui/system"
-import { NavBar } from "../components/NavBar";
+import { Box, Toolbar } from "@mui/material"
+import { SideBar, NavBar } from "../components";
 
-const drawerWidth = 240;
+const drawerWidth = 280;
 
 export const JournalLayout = ({children}) => {
   return (
@@ -9,12 +9,12 @@ export const JournalLayout = ({children}) => {
 
         <NavBar drawerWidth={drawerWidth}/>
 
-        {/*  Sidebar drawerWidth*/}
+        <SideBar drawerWidth={drawerWidth}/> 
         <Box 
         component='main'
         sx={{ flexGrow: 1, p: 3}}
         >
-            {/*  Toolbar */}
+            <Toolbar />
 
             {children}
         </Box>
