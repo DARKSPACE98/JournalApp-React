0a08fded2d7e53aaaecf5e44741a8beb18d8e951
DARKSPACE98 <enriquechuyin2019@gmail.com>
2023-05-03 02:54:16 -0700

    Se agrega componentes y NoteView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,18 @@
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views"
 
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-    <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque optio, veniam vel rem dicta alias quibusdam cupiditate mollitia ducimus voluptatem dignissimos delectus vitae sequi, quo animi beatae saepe ullam cum.</Typography>
-    
+{/*     <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque optio, veniam vel rem dicta alias quibusdam cupiditate mollitia ducimus voluptatem dignissimos delectus vitae sequi, quo animi beatae saepe ullam cum.</Typography>
+ */}    
+
     {/* NothingSelected */}
+    {/* <NothingSelectedView /> */}
     {/*  Noteview */}
+    <NoteView /> 
 
     </JournalLayout>
   )
